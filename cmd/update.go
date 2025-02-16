fda5d459dd7cfae428c8804be5d9d101d0101f0e
robinho46 <robin.yurt@hotmail.com>
2025-02-16 18:45:57 +0100

    feat: implemented feature to mark a task as completed fix: updated the menu with nicer charachters

---
+++ cmd/update.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func UpdateTask(db *sql.DB, taskId int) error {
+	query := "UPDATE tasks SET completed_at = NOW(), done = TRUE WHERE id = $1"
+
+	res, err := db.Exec(query, taskId)
+	if err != nil {
+		return fmt.Errorf("could not mark task as completed: %v", err)
+	}
+
+	// Check how many rows were affected
+	count, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("could not retrieve affected rows: %v", err)
+	}
+
+	if count == 0 {
+		return fmt.Errorf("no task found with id %d", taskId)
+	}
+
+	return nil
+
+}
